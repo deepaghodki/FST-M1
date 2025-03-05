2808bb1b77f6638518d7c83122dfe4eda8eeceb0
deepaghodki <63470354+deepaghodki@users.noreply.github.com>
2025-03-05 21:44:16 +0530

    Add files via upload

---
+++ Appium/Activities/ActionBase.java
@@ -0,0 +1,28 @@
+package Activity;
+
+
+import static java.time.Duration.ofMillis;
+import static org.openqa.selenium.interactions.PointerInput.MouseButton.LEFT;
+import static org.openqa.selenium.interactions.PointerInput.Origin.viewport;
+
+import java.awt.Point;
+import java.util.Arrays;
+
+import javax.sound.midi.Sequence;
+
+import org.openqa.selenium.interactions.PointerInput;
+
+import io.appium.java_client.AppiumDriver;
+
+public class ActionBase {
+    private final static PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+
+    public static void doSwipe(AppiumDriver driver, Point start, Point end, int duration) {
+        Sequence swipe = new Sequence(finger, 1)
+            .addAction(finger.createPointerMove(ofMillis(0), viewport(), start.getX(), start.getY()))
+            .addAction(finger.createPointerDown(LEFT.asArg()))
+            .addAction(finger.createPointerMove(ofMillis(duration), viewport(), end.getX(), end.getY()))
+            .addAction(finger.createPointerUp(LEFT.asArg()));
+        driver.perform(Arrays.asList(swipe));
+    }
+}
